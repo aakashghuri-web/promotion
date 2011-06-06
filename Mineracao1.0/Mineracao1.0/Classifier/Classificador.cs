200e5e899a02d311748727760b13715e4a517392
nina.alecrim88 <nina.alecrim88@b320efdd-3bbd-83c1-82a5-6b1666c6ca85>
2011-06-06 21:24:00 +0000

    moAR moar moar

---
+++ Mineracao1.0/Mineracao1.0/Classifier/Classificador.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MinWeb
+{
+    class Classificador
+    {       
+        public static int RT = 0;
+	    public static int PROPAGANDA = 1;
+	    public static int CADASTRO = 2;
+
+        public static int classificar(String texto){
+		int classe = -1;
+
+		//int de = texto.indexOf("de");
+		/*int rt = texto.indexOf(" rt ");
+		int concorra = texto.indexOf("concorra");*/
+		//int promo = texto.indexOf("promo");
+		//int oferta = texto.indexOf("oferta");
+		//int off = texto.indexOf("%off");
+		//int cadastro = texto.indexOf("cadastr");
+		//int ganhe = texto.indexOf("ganh");
+
+		if(texto.Contains(" rt ") && (texto.Contains("concorr")||texto.Contains("ganh"))){
+			classe = RT;
+		}
+		else if(texto.Contains("cadastr") && texto.Contains("ganh")){
+			classe = CADASTRO;
+		}		
+		else if(texto.Contains("%off")){
+			classe = PROPAGANDA;
+		}
+		else if(texto.Contains("promo") || texto.Contains("ofert")){			
+			
+			/*int gratis = texto.indexOf("gratis");
+			int free = texto.indexOf("free");
+			int desconto = texto.indexOf("desconto");*/
+			
+			
+			int de = texto.IndexOf("der$");
+			
+			if(texto.Contains("gratis") || texto.Contains("free")
+					||texto.Contains("desconto")){
+				classe = PROPAGANDA;
+			}
+			else if(de != - 1){
+				Console.WriteLine("der$ " + de);
+				
+				int i = de+4;
+
+				while(i < texto.Length &&ehDigito(texto[i])){
+					i++;
+				}
+
+				int por = texto.IndexOf("por", i);
+				Console.WriteLine("por " + por);
+				
+				if(por != -1 && (por -  i) <= 2){
+					Console.WriteLine("distancia der$dig - por " + (por - i));
+					classe = PROPAGANDA;
+				}
+			}
+
+			
+			
+		}
+		
+		if(classe != -1 && texto.Contains("http")){
+			int i = texto.IndexOf(" ", texto.IndexOf("http"));
+			
+			if(i != -1){
+				Console.WriteLine(texto.Substring(texto.IndexOf("http"), i));
+			}
+			else{
+				Console.WriteLine(texto.Substring(texto.IndexOf("http")));
+			}
+		}
+
+
+
+
+		return classe;
+	}
+
+        private static bool ehDigito(char c)
+        {
+            return (c == '0' || c == '1' || c == '2' ||
+                    c == '3' || c == '4' || c == '5' ||
+                    c == '6' || c == '7' || c == '8' ||
+                    c == '9');
+        }
+
+        static void Main()
+        {
+            String s = MensagemAux.pro7;
+		    Console.WriteLine(s);
+		    s = s.ToLower();
+		    s = Preprocessador.removerAcentuacao(s);
+	    	s = Preprocessador.removerPontuacao(s);
+    		s = Preprocessador.adaptarPreco(s);
+
+		    Console.WriteLine(classificar(s));
+        }
+
+    }
+}
---
+++ Mineracao1.0/Mineracao1.0/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using PromoTweet.Twitter;
+using System.Text;
+
+namespace Mineracao1._0
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+
+            TwitterSearch engine = new TwitterSearch();
+            List<TwitterEntry> result = engine.BuscaEntradaTwitter("promocao");
+            StringBuilder sb = new StringBuilder();
+            int id = 0;
+            foreach (TwitterEntry entry in result)
+            {
+                sb.Append("<div id=\"t\"" + id + "style=\"border-bottom-style:solid; border-bottom-color:Black;\" ");
+                    sb.Append("<div style=\"float:left; width:20%\">");
+                        sb.Append("<img height=\"48\" + width=\"48\" id=\"Image" + id + "\" src=\"" + entry.Avatar  + "\" title=\"" + entry.Author + "\" />");
+                    sb.Append("</div>");
+
+                    sb.Append("<div style=\"width:80%;padding-left:4px\"");
+                        sb.Append("<a id=\"a"+ id +"\" target=\"_blank\" href=\""+ entry.Uri + "\" class=\"linksTweet\">"+ entry.Author +"</a><br/>");
+                        sb.Append("<label id=\"l"+ id +"\">" + entry.Tweet + " </label>");
+                    sb.Append("</div>");
+                sb.Append("<br /></div>");
+                id++;
+            }
+
+            Label1.Text = sb.ToString();
+
+        }
+
+    }
+}
