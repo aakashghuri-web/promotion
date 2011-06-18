226552db3c239b889d3bff394562e3b18802f236
luizantoniocorreia@gmail.com <luizantoniocorreia@gmail.com@b320efdd-3bbd-83c1-82a5-6b1666c6ca85>
2011-06-18 19:25:36 +0000

    adicionado prioridade no resultado da busca e organizando o código

--- PromoTweet/Classifier/Classificador.cs
+++ PromoTweet/Classifier/Classificador.cs
@@ -13,53 +13,124 @@ namespace PromoTweet.Classificador
 	    public static int CADASTRO = 2;
 
         public static int classificar(String texto){
-            texto = texto.ToLower();
-            texto = Preprocessador.adaptarTexto(texto);
 
-		    int classe = -1;
+            texto = Preprocessador.preProcessamento(texto);
 
-            if ((((texto.Contains("de #rt")) && (texto.Contains("concorr") || texto.Contains("ganh") || texto.Contains("participe"))) || (texto.Contains("rt e concorra"))))
+            double importancia = 0;
+            if (texto.Contains("ganh"))
             {
-			    classe = RT;
-		    }
-            else if (texto.Contains("cadastr") && (texto.Contains("ganh") || texto.Contains("gratis")))
+                importancia = 1;
+            }
+            else if (texto.Contains("gratis") || texto.Contains("free"))
             {
-			    classe = CADASTRO;
-		    }		
-		    else if(texto.Contains("%off")){
+                importancia = 0.8;
+            }
+            else if (  texto.Contains("concorr")
+                    && texto.Contains("particip"))
+            {
+                importancia = 0.75;
+            }
+
+            double prioridadeRT = getPrioridadeRT(texto, importancia);
+            double prioridadeCadastro = getPrioridadeCadastro(texto, importancia);
+            double prioridadeDesconto = getPrioridadeDesconto(texto, importancia);
+
+            int classe = -1;
+
+            if (prioridadeCadastro != 0) {
+                classe = CADASTRO;
+            }
+            else if (prioridadeRT != 0) {
+                classe = RT;
+            }
+            else if (prioridadeDesconto != 0) {
                 classe = DESCONTO;
-		    }
-		    else if(texto.Contains("promo") || texto.Contains("ofert")){		
-			
-			    int de = texto.IndexOf("der$");
-			
-			    if(texto.Contains("gratis") || texto.Contains("free")
-					    ||texto.Contains("desconto")){
-                            classe = DESCONTO;
-			    }
-			    else if(de != - 1){
-				    Console.WriteLine("der$ " + de);
-				
-				    int i = de+4;
-
-				    while(i < texto.Length &&ehDigito(texto[i])){
-					    i++;
-				    }
-
-				    int por = texto.IndexOf("por", i);
-				    Console.WriteLine("por " + por);
-				
-				    if(por != -1 && (por -  i) <= 2){
-					    Console.WriteLine("distancia der$dig - por " + (por - i));
-					    classe = DESCONTO;
-				    }
-			    }
-
-			
-			
-		    }
-		    return classe;
+            }
+            
+		    return 0;
 	}
+        /*se o texto possuir a classe RT,
+         * retorna uma prioridade > 0;
+         * se não possuir, retorna 0
+         */
+        private static double getPrioridadeRT(string texto, double importancia)
+        {
+            double prioridade = 0;
+            
+            if(texto.Contains("de #rt")&& (importancia != 0)){
+                prioridade = 100*importancia;
+            }
+            else if(texto.Contains("rt e concorr")){
+                prioridade = 60;
+            }
+            else if (texto.Contains("#rt") && (importancia != 0))
+            {
+                prioridade = 50 * importancia;
+            }
+            
+            return prioridade;            
+        }
+
+        /*se o texto possuir a classe CADASTRO,
+         * retorna uma prioridade > 0;
+         * se não possuir, retorna 0.
+         */
+        private static double getPrioridadeCadastro(string texto, double importancia)
+        {
+            double prioridade = 0;
+
+            if (texto.Contains("cadastr"))
+            {
+                prioridade = 1 * importancia;
+            }           
+
+            return prioridade;
+        }
+
+        /*se o texto possuir a classe DESCONTO,
+         * retorna uma prioridade > 0;
+         * se não possuir, retorna 0.
+         */
+        private static double getPrioridadeDesconto(string texto, double importancia)
+        {
+            double prioridade = 0;
+
+            if (texto.Contains("%off"))
+            {
+                prioridade = 80;
+            }
+            else if (texto.Contains("promo") || texto.Contains("ofert"))
+            { 
+                int de = texto.IndexOf("der$");
+
+                if (de != -1)
+                {
+                    Console.WriteLine("der$ " + de);
+
+                    int i = de + 4;
+
+                    while (i < texto.Length && ehDigito(texto[i]))
+                    {
+                        i++;
+                    }
+
+                    int por = texto.IndexOf("por", i);
+                    //Console.WriteLine("por " + por);
+
+                    if (por != -1 && (por - i) <= 2)
+                    {
+                        //Console.WriteLine("distancia der$dig - por " + (por - i));
+                        prioridade = 100;
+                    }
+                }
+                else {
+                    prioridade = 80 * importancia;
+                
+                }
+            }
+
+            return prioridade;
+        }
 
         private static bool ehDigito(char c)
         {
